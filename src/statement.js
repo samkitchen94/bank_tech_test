6ada1769ac960715acfbdd345fde2431d69bea20
Sam Kitchen <samkitchen94@gmail.com>
2019-07-01 17:04:54 +0100

    show transactions test now passing

--- src/statement.js
+++ src/statement.js
@@ -3,13 +3,23 @@ function Statement(account) {
 };
 
 Statement.prototype = {
-  formatDate(date) {
-    var date = new Date(date);
-    var day = (date.getDate()).toString();
-    var month = (date.getMonth()).toString();
-    var year = (date.getFullYear()).toString();
-    return day + "/" + month + "/" + year;
+  formatTransactions(transaction) {
+    if (transaction['amount'] > 0) {
+      return `${(transaction['date'])} || ${(transaction['amount']).toFixed(2)} || || ${transaction['balance'].toFixed(2)}`
+    }
+    else {
+    return `${(transaction['date'])} || || ${(transaction['amount'] * -1).toFixed(2)} || ${transaction['balance'].toFixed(2)}`
+    }
   },
+
+  showTransactions() {
+    var header = "date || credit || debit || balance\n"
+    for(var i = this.account.getTransactions().length; i > 0; i--) {
+      header += this.formatTransactions(this.account.getTransactions()[i - 1]) + '\n';
+    }
+    return header;
+  },
+
 
 
 };
