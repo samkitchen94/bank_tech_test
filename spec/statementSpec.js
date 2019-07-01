8348fd569baa423ddf830d8fe7cc62e611b0c372
Sam Kitchen <samkitchen94@gmail.com>
2019-07-01 16:20:23 +0100

    it formats the date

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -12,4 +12,21 @@ describe("Bank Statement", function() {
       expect(statement.account.balance).toEqual(0)
     });
   });
+
+  describe("formatDate", function() {
+    it("formats a date so it does not show time and gmt etc", function() {
+      var date = new Date('03/12/2019')
+      expect(statement.formatDate(date)).toEqual('12/2/2019')
+    });
+  });
+  // describe("showTransactions", function() {
+  //   it("shows a list of previous transactions", function() {
+  //     statement.account.deposit(100.00, 12/07/2019)
+  //     statement.account.withdraw(50.00, 13/07/2019)
+  //     expect(statement.showTransactions()).toEqual(
+  //       "date || credit || debit || balance \n
+  //       12/07/2019 || || 100.00 || 100.00 \n
+  //       13/07/2019 || 50.00 || || 50.00")
+  //   });
+  // });
 });
--- src/statement.js
+++ src/statement.js
@@ -1,3 +1,15 @@
 function Statement(account) {
   this.account = account;
 };
+
+Statement.prototype = {
+  formatDate(date) {
+    var date = new Date(date);
+    var day = (date.getDate()).toString();
+    var month = (date.getMonth()).toString();
+    var year = (date.getFullYear()).toString();
+    return day + "/" + month + "/" + year;
+  },
+
+
+};
